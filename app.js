a898f192cb298e7910a0872bd663bbc9789cefe9
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-23 14:33:40 +0100

    feat: ADD static files middleware on App.js

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const errorsHandler = require("./middleware/errorsHandler");
 const app = express();
 const port = process.env.SERVER_PORT;
 
+// Static Files middleware
+app.use(express.static(`public`));
+
 // Router Movies
 app.use("/movies", moviesRouter);
-
 
 // Error Handler middleware
 app.use(errorsHandler);
 
+// Server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
