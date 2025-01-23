39d9c1fa51af3249597adbf68870c7fa4dd99ca9
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-23 15:17:08 +0100

    update: Created show route for movies and reviews

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -17,8 +17,38 @@ const index = (req, res, next) => {
 
 //SHOW
 const show = (req, res) => {
-    res.json({
-        message: "Show Movies"
+    const id = req.params.id;
+    const sql = "SELECT * FROM `movies` WHERE `id` = ?";
+    const sqlReviews = `
+      SELECT reviews.* 
+      FROM reviews
+      JOIN movies
+      ON movies.id = reviews.movie_id
+      WHERE movies.id = ?
+    `;
+
+    dbConnection.query(sql, [id], (err, results) => {
+        if (err) {
+            return next(new Error("Internal Server Error"))
+        }
+        if (results.length === 0) {
+            return res.status(404).json({
+                status: "fail",
+                message: "Movie not found",
+            });
+        }
+        dbConnection.query(sqlReviews, [id], (err, reviews) => {
+            if (err) {
+                return next(new Error("Internal Server Error"))
+            }
+            return res.status(200).json({
+                status: "succes",
+                data: {
+                    ...results[0],
+                    reviews,
+                },
+            });
+        });
     });
 };
 
