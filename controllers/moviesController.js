7895a242d8e6be85d575511f48981a37c64bd877
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-23 14:53:24 +0100

    update: Created index route for movies

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,7 +1,17 @@
+const dbConnection = require("../Data/dbConnection");
+
 //INDEX
-const index = (req, res) => {
-    res.json({
-        message: "Index Movies"
+const index = (req, res, next) => {
+    const sql = "SELECT * FROM `movies`";
+
+    dbConnection.query(sql, (err, movies) => {
+        if (err) {
+            return next(new Error("Internal Server Error"))
+        }
+        return res.status(200).json({
+            status: "success",
+            data: movies,
+        });
     });
 };
 
