511d358bfe79c153f5623cb2883d8892c8060b4c
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-27 14:15:50 +0100

    feat: ADD sql and seeder for slug column

---
+++ Data/2025-01-27-db-seeder-slug.sql
@@ -0,0 +1,5 @@
+UPDATE `movies_db`.`movies` SET `slug` = 'inception' WHERE (`id` = '1');
+UPDATE `movies_db`.`movies` SET `slug` = 'the_godfather' WHERE (`id` = '2');
+UPDATE `movies_db`.`movies` SET `slug` = 'titanic' WHERE (`id` = '3');
+UPDATE `movies_db`.`movies` SET `slug` = 'the_matrix' WHERE (`id` = '4');
+UPDATE `movies_db`.`movies` SET `slug` = 'interstellar' WHERE (`id` = '5');
