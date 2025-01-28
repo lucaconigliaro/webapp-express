22efece33a7d7e63b4692cf36d47b890366c5ebb
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-28 14:12:15 +0100

    chore: FIX typo in comment

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -82,7 +82,7 @@ const storeReview = (req, res, next) => {
         });
     }
 
-    //Valitadion name
+    //Validation name
     if (name.length <= 3) {
         return res.status(400).json({
             status: "fail",
